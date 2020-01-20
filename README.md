29cd97fc62ea19455b4c431d1c10aef86a271994
kokuyouwind <kokuyouwind@gmail.com>
2020-01-20 22:43:55 +0900

    implement array pattern demo

---
+++ README.md
@@ -0,0 +1,14 @@
+Pattern Match Demo
+=====
+
+[Burikaigi 2020](https://toyama-eng.connpass.com/event/156635/) で発表するデモのソースコードです。
+
+Rubyパターンマッチの基本的な機能と、[ActivePattern gem](https://github.com/kokuyouwind/active_pattern)及び[MethodMatchable gem](https://github.com/kokuyouwind/method_matchable)の使い方を含みます。
+
+## Usage
+
+標準ではパターンマッチに関する警告が大量にでるので、`-W0`オプションを付けて実行するのが望ましいです。
+
+```bash
+$ ruby -W0 src/01_array.rb
+```
---
+++ src/01_array.rb
@@ -0,0 +1,32 @@
+# 配列の要素ごとにパターンマッチできる
+puts "basic pattern"
+case [1, 2, 3]
+in [2, _, _]
+  fail
+in [1, x, y]
+  puts "x: #{x}, y: #{y}" # => x: 2, y: 3
+end
+
+# * を使って残りとマッチできる
+puts "asterisk pattern"
+case [1, 2, 3]
+in [x, *y]
+  puts "x: #{x}, y: #{y}" # => x: 1, y: [2, 3]
+end
+
+# 1行でinを使ってパターンマッチできる
+puts "oneline pattern"
+[1, 2, 3] in [x, _, _]
+puts "x: #{x}" # => x: 1
+
+# destructを定義してればなんでもマッチできる
+puts "deconstruct with method"
+class Tester
+  def self.deconstruct
+    [1, 2, 3]
+  end
+end
+case Tester
+in [_, x, _]
+  puts "x: #{x}" # => x: 2
+end
