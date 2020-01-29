1619842dfabdc4e49396b53d83e2a431db3cf801
kokuyouwind <kokuyouwind@gmail.com>
2020-01-29 22:39:16 +0900

    rename class to constant

--- src/03_constant.rb
+++ src/03_constant.rb
@@ -1,5 +1,5 @@
-# クラスを指定してマッチできる
-puts "match with class"
+# 定数を指定してマッチできる
+puts "match with constant"
 case 1
 in String
   fail # マッチしない
@@ -7,23 +7,23 @@ in Integer
   puts "1 is Integer"
 end
 
-# クラスと配列を合わせて指定できる
-puts "array with class"
+# 定数と配列を合わせて指定できる
+puts "array with constant"
 Point = Struct.new(:x, :y)
 case Point.new(1, 2)
 in Point[x, y]
   puts "x: #{x}, y: #{y}" # => x: 1, y: 2
 end
 
-# クラスとハッシュを合わせて指定できる
-puts "hash with class"
+# 定数とハッシュを合わせて指定できる
+puts "hash with constant"
 case Point.new(3, 4)
 in Point(x: x, y: y)
   puts "x: #{x}, y: #{y}" # => x: 3, y: 4
 end
 
 # ===を定義すればマッチ条件を変えられる
-# `case` ではなく `in` に書かれたクラスのほうが使われる
+# `case` ではなく `in` (パターン側)が使われる
 puts "define === method"
 class Tester
   def self.===(other)
